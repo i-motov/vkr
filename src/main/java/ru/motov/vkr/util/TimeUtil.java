35f3bf9bc72365c5b709cac10acf45a69e514d5b
Igor Motov <f3brua@gmail.com>
2021-06-14 15:56:09 +0300

    2_1

--- src/main/java/ru/motov/vkr/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package ru.motov.vkr.util;
-
-import java.time.LocalTime;
-
-public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-}

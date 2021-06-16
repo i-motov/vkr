0f41aee76f9fc11819a9862c7454c23e8144f15f
Igor Motov <f3brua@gmail.com>
2021-06-16 12:51:52 +0300

    4_8_add_jpa

---
+++ src/main/java/ru/motov/vkr/repository/jpa/JpaMealRepository.java
@@ -0,0 +1,37 @@
+package ru.motov.vkr.repository.jpa;
+
+import org.springframework.stereotype.Repository;
+import ru.motov.vkr.model.Meal;
+import ru.motov.vkr.repository.MealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class JpaMealRepository implements MealRepository {
+
+    @Override
+    public Meal save(Meal meal, int userId) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return false;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return null;
+    }
+}

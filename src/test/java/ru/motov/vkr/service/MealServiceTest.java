832fce0ebb2806a6b8105626142a9cb8b9d61821
Igor Motov <f3brua@gmail.com>
2021-06-17 00:52:24 +0300

    5_5_profiles_connection_pool

--- src/test/java/ru/motov/vkr/service/MealServiceTest.java
+++ src/test/java/ru/motov/vkr/service/MealServiceTest.java
@@ -9,11 +9,13 @@ import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.motov.vkr.Profiles;
 import ru.motov.vkr.model.Meal;
 import ru.motov.vkr.util.exception.NotFoundException;
 
@@ -33,6 +35,7 @@ import static ru.motov.vkr.UserTestData.USER_ID;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(Profiles.ACTIVE_DB)
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
--- src/test/java/ru/motov/vkr/service/UserServiceTest.java
+++ src/test/java/ru/motov/vkr/service/UserServiceTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.motov.vkr.Profiles;
 import ru.motov.vkr.UserTestData;
 import ru.motov.vkr.model.Role;
 import ru.motov.vkr.model.User;
@@ -24,6 +26,7 @@ import static ru.motov.vkr.UserTestData.*;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(Profiles.ACTIVE_DB)
 public class UserServiceTest {
 
     @Autowired

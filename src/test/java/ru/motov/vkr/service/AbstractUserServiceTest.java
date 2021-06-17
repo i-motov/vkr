16596a44454cc65598a547044a9aa88f37b8f7ed
Igor Motov <f3brua@gmail.com>
2021-06-17 14:05:54 +0300

    6_09_hibernate_cache

--- src/test/java/ru/motov/vkr/service/AbstractUserServiceTest.java
+++ src/test/java/ru/motov/vkr/service/AbstractUserServiceTest.java
@@ -14,6 +14,7 @@ import javax.validation.ConstraintViolationException;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import ru.motov.vkr.repository.JpaUtil;
 
 import static org.junit.Assert.assertThrows;
 import static ru.motov.vkr.UserTestData.*;
@@ -26,9 +27,13 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     private CacheManager cacheManager;
 
+    @Autowired
+    protected JpaUtil jpaUtil;
+
     @Before
     public void setup() {
         cacheManager.getCache("users").clear();
+        jpaUtil.clear2ndLevelHibernateCache();
     }
 
     @Test

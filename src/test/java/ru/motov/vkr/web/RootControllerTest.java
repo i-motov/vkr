a22e7f76db758de8d3ebba5b4b74a5b110a5fe0d
Igor Motov <f3brua@gmail.com>
2021-06-18 00:44:38 +0300

    7_09_controller_test

---
+++ src/test/java/ru/motov/vkr/web/RootControllerTest.java
@@ -0,0 +1,29 @@
+package ru.motov.vkr.web;
+
+import org.junit.Test;
+import ru.motov.vkr.UserTestData;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.motov.vkr.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void getUsers() throws Exception {
+        perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(UserTestData.user.getName()))
+                        )
+                )));
+    }
+}

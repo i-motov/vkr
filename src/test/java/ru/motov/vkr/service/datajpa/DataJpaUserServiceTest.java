e0da81313e3febbf4508336a7e4d44b221c917d2
Igor Motov <f3brua@gmail.com>
2021-06-17 23:12:51 +0300

    7_04_optional_add_role

--- src/test/java/ru/motov/vkr/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/motov/vkr/service/datajpa/DataJpaUserServiceTest.java
@@ -10,16 +10,16 @@ import ru.motov.vkr.service.AbstractUserServiceTest;
 import ru.motov.vkr.util.exception.NotFoundException;
 
 import static ru.motov.vkr.Profiles.DATAJPA;
-import static ru.motov.vkr.UserTestData.USER_ID;
+import static ru.motov.vkr.UserTestData.ADMIN_ID;
 import static ru.motov.vkr.UserTestData.USER_MATCHER;
 
 @ActiveProfiles(DATAJPA)
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
     @Test
     public void getWithMeals() {
-        User user = service.getWithMeals(USER_ID);
-        USER_MATCHER.assertMatch(user, UserTestData.user);
-        MealTestData.MEAL_MATCHER.assertMatch(user.getMeals(), MealTestData.meals);
+        User admin = service.getWithMeals(ADMIN_ID);
+        USER_MATCHER.assertMatch(admin, UserTestData.admin);
+        MealTestData.MEAL_MATCHER.assertMatch(admin.getMeals(), MealTestData.adminMeal2, MealTestData.adminMeal1);
     }
 
     @Test

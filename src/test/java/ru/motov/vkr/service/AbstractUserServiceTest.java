7d8eb0ec8ce4a14175c4e6eea63825fb6a3a4c64
Igor Motov <f3brua@gmail.com>
2021-06-17 13:18:27 +0300

    6_08_add_test_validation

--- src/test/java/ru/motov/vkr/service/AbstractUserServiceTest.java
+++ src/test/java/ru/motov/vkr/service/AbstractUserServiceTest.java
@@ -10,7 +10,10 @@ import ru.motov.vkr.model.Role;
 import ru.motov.vkr.model.User;
 import ru.motov.vkr.util.exception.NotFoundException;
 
+import javax.validation.ConstraintViolationException;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertThrows;
 import static ru.motov.vkr.UserTestData.*;
@@ -83,5 +86,14 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     public void getAll() {
         List<User> all = service.getAll();
         USER_MATCHER.assertMatch(all, admin, user);
+    }
+
+    @Test
+    public void createWithException() throws Exception {
+        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.USER)));
+        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "  ", "password", Role.USER)));
+        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.USER)));
+        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Set.of())));
+        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Set.of())));
     }
 }

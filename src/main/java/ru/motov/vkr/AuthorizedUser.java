5b25fcef60df1719dfab690149f239566c69dc34
Igor Motov <f3brua@gmail.com>
2021-06-18 23:29:34 +0300

    9_11_auth_via_user_service

---
+++ src/main/java/ru/motov/vkr/AuthorizedUser.java
@@ -0,0 +1,36 @@
+package ru.motov.vkr;
+
+import ru.motov.vkr.model.User;
+import ru.motov.vkr.to.UserTo;
+import ru.motov.vkr.util.UserUtil;
+
+import java.io.Serial;
+
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UserTo userTo;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = UserUtil.asTo(user);
+    }
+
+    public int getId() {
+        return userTo.id();
+    }
+
+    public void update(UserTo newTo) {
+        userTo = newTo;
+    }
+
+    public UserTo getUserTo() {
+        return userTo;
+    }
+
+    @Override
+    public String toString() {
+        return userTo.toString();
+    }
+}

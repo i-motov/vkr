d9e37c765e4c2b44952c632214580d53545b3c68
Igor Motov <f3brua@gmail.com>
2021-06-19 11:44:55 +0300

    10_01_binding_ajax

--- src/main/java/ru/motov/vkr/web/RootController.java
+++ src/main/java/ru/motov/vkr/web/RootController.java
@@ -1,17 +1,10 @@
 package ru.motov.vkr.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.motov.vkr.service.MealService;
-import ru.motov.vkr.util.MealsUtil;
 
 @Controller
 public class RootController {
-
-    @Autowired
-    private MealService mealService;
 
     @GetMapping("/")
     public String root() {
@@ -29,9 +22,7 @@ public class RootController {
     }
 
     @GetMapping("/meals")
-    public String getMeals(Model model) {
-        model.addAttribute("meals",
-                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
+    public String getMeals() {
         return "meals";
     }
 }
